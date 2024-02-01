5f29a48522a18edeee22baa0c2b72ce001f08d4f
JOYSTROMBECK <120531019+JOYSTROMBECK@users.noreply.github.com>
2024-01-31 22:19:33 -0500

    d

--- src/Weather.js
+++ src/Weather.js
@@ -8,20 +8,7 @@ export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
-      wind: response.data.wind.speed,
-      city: response.data.name,
-    });
-  }
-
+  
   function handleSubmit(event) {
     event.preventDefault();
     search();
